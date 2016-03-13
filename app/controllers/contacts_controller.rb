8635dc2292b2a3e91789ada4b3cc39af2be127c7
Dana <danasblack@gmail.com>
2016-03-13 13:12:12 -0500

    contacts app original data

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,38 @@
+class ContactsController < ApplicationController
+  def index  
+    @contacts = Contact.all
+    render "index.html.erb"
+  end
+
+  def new
+    render 'new.html.erb'  
+  end
+  
+  def create
+    Contact.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    render 'create.html.erb'
+  end 
+
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render 'show.html.erb'
+  end  
+
+  def edit
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    Contact.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    render 'create.html.erb'
+end 
+
